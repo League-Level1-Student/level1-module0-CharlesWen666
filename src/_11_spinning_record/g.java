d16c5fe756dc27450fc6d6eefacc1a520c8d5610
wench <wench@LAPTOP-MSGNLGK6.lan>
2021-03-19 16:58:48 -0700

    we

---
+++ src/_11_spinning_record/g.java
@@ -0,0 +1,12 @@
+package _11_spinning_record;
+
+public class g {
+	
+	static void jump(String obstacle) {
+
+        System.out.println("The mouse jumped over the " + obstacle);
+ }
+	public static void main(String[] args) {
+		jump("piggy");
+	} }
+
